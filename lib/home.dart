83aa4b7d402557971d904e22b0d86822b65e44fe
EA - Denilson De Sousa Soares <denilson_sousa@educadventista.org>
2023-05-10 21:12:59 -0300

    Finalizado

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'screens/explore_screen.dart';
+
+import 'screens/recipes_screen.dart';
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -11,10 +15,9 @@ class HomeState extends State<Home> {
   int _selectedIndex = 0;
 
   static List<Widget> pages = <Widget>[
-    // TODO: Replace with ExploreScreen
+    ExploreScreen(),
+    RecipesScreen(),
     Container(color: Colors.white),
-    // TODO: Replace with RecipesScreen
-    Container(color: Colors.green),
     Container(color: Colors.blue),
   ];
 
